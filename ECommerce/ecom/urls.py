abf49528637e06844f1015b596ac83705ee242e4
Santanu kumar <8579santanu@gmail.com>
2026-02-13 15:07:27 +0530

    Update product model and category view

--- ECommerce/ecom/urls.py
+++ ECommerce/ecom/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("category/", views.CategoryView.as_view(), name="category")
 ]
--- ECommerce/ecom/views.py
+++ ECommerce/ecom/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request, "app/home.html")
 
+class CategoryView(View):
+    def get(self, request):
+        return render(request, "app/category.html")
